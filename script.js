93eadd3ed558babe199140ebb3bb590b951216b2
alfian <alfiansyahputra911@gmail.com>
2023-10-09 01:35:28 +0700

    versi 1 love

---
+++ script.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+    $("#btn-1").on("click", function() {
+        $(".content2").css("display", "block");
+        $(".content1").css("display", "none");
+    })
+    $("#btn-2").on("click", function() {
+        $(".content3").css("display", "block");
+        $(".content2").css("display", "none");
+    })
+    $("#btn-3").on("click", function() {
+        $(".content4").css("display", "block");
+        $(".content3").css("display", "none");
+    })
+    $("#btn-4").on("click", function() {
+        $(".content5").css("display", "block");
+        $(".content4").css("display", "none");
+    })
+    $("#btn-5").on("click", function() {
+        $(".content6").css("display", "block");
+        $(".content5").css("display", "none");
+    })
+    $("#btn-6").on("click", function() {
+        $(".content7").css("display", "block");
+        $(".content6").css("display", "none");
+    })
+    $("#btn-7").on("click", function() {
+        $(".content8").css("display", "block");
+        $(".content7").css("display", "none");
+    })
+    $("#btn-8").on("click", function() {
+        $(".content9").css("display", "block");
+        $(".content8").css("display", "none");
+    })
+    $("#btn-9").on("click", function() {
+        $(".content10").css("display", "block");
+        $(".content9").css("display", "none");
+    })
+})
